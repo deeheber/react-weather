f772d4d1e6d14eb218d39662edacf36face0d48f
Danielle H <daheber@gmail.com>
2016-12-10 13:40:41 -0800

    get input data

--- app/components/GetCity.js
+++ app/components/GetCity.js
@@ -1,18 +1,25 @@
 var React = require('react');
+var PropTypes = React.PropTypes;
 
 //TODO: refactor inline style for margin later
 
 function GetCity(props){
     return (
-        <form>
+        <form onSubmit={props.onSubmitCity}>
             <div className='col-sm-6 col-sm-offset-3' >
-                <input className='form-control input-md' placeholder='Enter a City and State' type='text'/>
+                <input className='form-control input-md' placeholder='Enter a City and State' type='text' value={props.city} onChange={props.onUpdateCity} />
             </div>
             <div className='col-sm-12' style={{marginTop: '5px'}}>
-                <button className='btn btn-info btn-md'>Get Weather</button>
+                <button className='btn btn-info btn-md' type='submit'>Get Weather</button>
             </div>    
         </form>
     )
 }
+
+GetCity.propTypes = {
+    city: PropTypes.string.isRequired,
+    onUpdateCity: PropTypes.func.isRequired,
+    onSubmitCity: PropTypes.func.isRequired
+};
 
 module.exports = GetCity;
--- app/containers/GetCityContainer.js
+++ app/containers/GetCityContainer.js
@@ -2,9 +2,30 @@ var React = require('react');
 var GetCity = require('../components/GetCity');
 
 var GetCityContainer = React.createClass({
+    getInitialState: function(){
+        return {
+            city: ''
+        }
+    },
+    handleUpdateCity: function(e){
+        this.setState({
+            city: e.target.value
+        });
+    },
+    handleSubmitCity: function(e){
+        e.preventDefault();
+        console.log(this.state.city);
+        //TODO later
+            //empty out input
+            //redirect to forecast component
+    },
     render: function(){
         return (
-            <GetCity />
+            <GetCity 
+                city={this.state.city}
+                onUpdateCity={this.handleUpdateCity}
+                onSubmitCity={this.handleSubmitCity}
+            />
         )
     }
 });
