5b037f2c91793723166b5f300c49ce874baa7256
Danielle H <daheber@gmail.com>
2016-12-15 15:47:51 -0800

    extract text plugin to handle css

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,44 @@
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-    devtool: 'source-map',
     entry: [
         './app/index.js'
     ],
     output: {
         path: __dirname + '/dist',
-        filename: 'index_bundle.js'
+        filename: 'bundle.js'
     },
-    module: {
-        loaders: [
-          {
-            test: /\.js$/, 
-            include: __dirname + '/app', 
-            loader: 'babel-loader',
-            query: {
-              presets: ['es2015'],
-              cacheDirectory: true
-            }
-          },
-          {
-            test: /\.css$/, 
-            loader: 'style-loader!css-loader'
-          }
-        ]
-    },
+    devtool: 'source-map',
     plugins: [
       new HtmlWebpackPlugin({
           template: __dirname + '/app/index.html',
           filename: 'index.html',
           inject: 'body'
       }),
+      new ExtractTextPlugin('/styles/bundle.css'),
       new webpack.DefinePlugin({
         'process.env': {
             NODE_ENV: JSON.stringify('production')
         }
       }),
       new webpack.optimize.UglifyJsPlugin()
-    ]
+    ],
+    module: {
+        loaders: [
+          {
+            test: /\.js$/, 
+            include: __dirname + '/app', 
+            loader: 'babel',
+            query: {
+              presets: ['es2015', 'react']
+            }
+          },
+          {
+            test: /\.css$/, 
+            loader: ExtractTextPlugin.extract('style!', 'css?sourceMap')
+          }
+        ]
+    }
 }
