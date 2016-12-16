9b6459e739016c3d39b999ebe38b42defa9baeed
Danielle H <daheber@gmail.com>
2016-12-15 16:23:08 -0800

    detail container and component initial setup

---
+++ app/components/Detail.js
@@ -0,0 +1,16 @@
+var React = require('react');
+var PropTypes = React.PropTypes;
+
+function Detail(props){
+  return (
+    <div>
+      This is the detail component.
+    </div>
+  )
+}
+
+Detail.propTypes = {
+
+};
+
+module.exports = Detail;
