476fd1a4d2ec943e9cd4010727bf1a89a71fa1f7
Danielle H <daheber@gmail.com>
2016-12-17 10:39:25 -0800

    es6 for routes

--- app/index.js
+++ app/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import routes from './config/routes';
+import {routes} from './config/routes';
 import './styles/index.css';
 
 ReactDOM.render(routes, document.getElementById('app'));
